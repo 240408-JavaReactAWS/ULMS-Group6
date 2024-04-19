698686132953941b901eac61d9ab635df78fdd6a
MamoonUddin <mamoonuddin17@gmail.com>
2024-04-19 15:57:51 -0500

    Attempt 2

---
+++ back-end/src/test/java/com/revature/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
