351095e9fbb0133d27c126c6b71dc2248dfe3f49
sir-shogun <kwt7311@comcast.net>
2024-04-23 16:09:32 -0400

    User Login Functionality

--- back-end/src/main/java/com/revature/backend/repos/UsersDAO.java
+++ back-end/src/main/java/com/revature/backend/repos/UsersDAO.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsersDAO extends JpaRepository<Users, Integer> {
     Users findByUsername(String username);
+
+    boolean findUser(String useName, String pword);
 }
--- back-end/src/main/java/com/revature/backend/services/UsersService.java
+++ back-end/src/main/java/com/revature/backend/services/UsersService.java
@@ -4,6 +4,7 @@ package com.revature.backend.services;
 import com.revature.backend.repos.UsersDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.revature.backend.models.Users;
 
 @Service
 public class UsersService {
@@ -12,5 +13,9 @@ public class UsersService {
     @Autowired
     public UsersService(UsersDAO usersDAO) {
         this.usersDAO = usersDAO;
+    }
+
+    public boolean login (Users user) {
+        return usersDAO.findUser(user.getUsername(), user.getPassword());
     }
 }
