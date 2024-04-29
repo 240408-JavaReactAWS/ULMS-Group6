38ec657ec6aefeb71a5c1e515d99d7d6042bb2ad
sir-shogun <kwt7311@comcast.net>
2024-04-29 16:14:19 -0400

    Added student dashboard functionality

--- front-end/src/Components/CourseCards/CourseCard.tsx
+++ front-end/src/Components/CourseCards/CourseCard.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { ICourse } from "../Models/ICourse";
+import Course from "../../interfaces/CourseInterface";
+import { useNavigate, useParams } from "react-router-dom";
 
-function CourseCard(props: ICourse) {
+function CourseCard(props: Course) {
+
+    const id = props.courseId;
+
+    const navigate = useNavigate();
+
+    let viewCourse = () => {
+        navigate(`/courses/${id}`);
+    }
+
     return (
         <div className = 'Course'>
             <h1>{props.courseName}</h1>
             <h2>{props.courseId}</h2>
+            <button onClick={viewCourse}>View Course</button>
         </div>
     )
 }
